a7995d959e69e43a134242c1dc4299c6bac1d631
fauzilula <fauzil.ula@ui.ac.id>
2022-09-22 15:26:00 +0700

    Lab 3

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,43 @@
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render
-from django.shortcuts import render
 from wishlist.models import BarangWishlist
-...
+
+
 # Create your views here.
+@login_required(login_url='/wishlist/login/')
+def show_wishlist(request):
+     data_barang_wishlist = BarangWishlist.objects.all()
+     context = {
+         'list_barang': data_barang_wishlist,
+         'nama': 'Fauzil Ula Fachrudin',
+         'last_login': request.COOKIES['last_login'],
+     }
+     return render(request, "wishlist.html", context)
+
+    
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('wishlist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+    
 def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
@@ -21,10 +54,24 @@ def show_xml_by_id(request,id):
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
          
-def show_wishlist(request):
-     data_barang_wishlist = BarangWishlist.objects.all()
-     context = {
-         'list_barang': data_barang_wishlist,
-         'nama': 'Fauzil Ula Fachrudin'
-     }
-     return render(request, "wishlist.html", context)
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return redirect('wishlist:show_wishlist')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+    
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('wishlist:login'))
+    response.delete_cookie('last_login')
+    return redirect('wishlist:login')
+    
